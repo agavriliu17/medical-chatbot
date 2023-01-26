8c19183b70b923b2b5b5222b4a88438c00c8531b
raresbancescu <75622881+raresbancescu@users.noreply.github.com>
2023-01-26 18:51:40 +0200

    Classification

--- server/main.py
+++ server/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException, UploadFile
+from fastapi import FastAPI, HTTPException, UploadFile, File
 from pydantic import BaseModel
 import uuid
 import json
@@ -7,9 +7,13 @@ import openai
 from dotenv import load_dotenv
 import datetime
 import re
+import cv2
+import tensorflow as tf
+from keras.models import load_model
+from PIL import Image
+import numpy as np
+
 from fastapi.middleware.cors import CORSMiddleware
-
-
 
 app = FastAPI()
 app.add_middleware(
@@ -21,7 +25,9 @@ app.add_middleware(
 )
 
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
+openai.api_key = "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"
+print(openai.api_key)
+
 
 # TODO: Rename this
 class Item(BaseModel):
@@ -79,7 +85,8 @@ def completion(conversation_id: str, item: Item):
     print(prompt)
     try:
         response = openai.Completion.create(model="text-davinci-003", prompt=prompt, temperature=0.2,
-                                            max_tokens=100, top_p=0.5, frequency_penalty=1.65, presence_penalty=0.6, stop=["User: "]  )
+                                            max_tokens=100, top_p=0.5, frequency_penalty=1.65, presence_penalty=0.6,
+                                            stop=["User: "])
         completion = response["choices"][0]["text"].replace("\n\n", "")
         completion = re.sub(r"Bot: ", "", completion)
 
@@ -90,12 +97,80 @@ def completion(conversation_id: str, item: Item):
         }
         conversations[conversation_id].append(botResponse)
     except Exception as e:
+        print(e)
         raise HTTPException(status_code=500, detail="OpenAI API error")
 
     with open(file_name, "w") as file:
         json.dump(conversations, file, default=str)
     return botResponse
 
+
 @app.post("/upload_file/")
-async def create_upload_file(file: UploadFile):
-    return {"filename": file.filename}
+async def create_upload_file(file: UploadFile = File(...)):
+    file_name = file.filename
+    file_path = os.path.join(
+        r"D:\facultate\Anul 3\Semestrul 1\Inteligenta Artificiala\Medical chatbot\medical-chatbot\files", file_name)
+    with open(file_path, "wb") as f:
+        f.write(file.file.read())
+        f.close()
+    INPUT_SIZE = 150
+    model = load_model(
+        r"D:\facultate\Anul 3\Semestrul 1\Inteligenta Artificiala\Medical chatbot\medical-chatbot\server\skincancer\skincancer.h5")
+    image = cv2.imread(
+        file_path)
+    img = Image.fromarray(image)
+    img = img.resize((INPUT_SIZE, INPUT_SIZE))
+    img = np.array(img)
+    input_image = np.expand_dims(img, axis=0)
+    result = model.predict(input_image)
+    index = 0
+    max = 0
+    print(result)
+    for i in range(0, len(result[0])):
+        if result[0][i] > max:
+            max = result[0][i]
+            index = i
+    print(result)
+    disease_predict = ""
+    if index == 0:
+        disease_predict = "Actinic keratosis"
+    elif index == 1:
+        disease_predict = "Basal cell carcinoma"
+    elif index == 2:
+        disease_predict = "Dermatofibroma"
+    elif index == 3:
+        disease_predict = "Melanoma"
+    elif index == 4:
+        disease_predict = "Nevus"
+    elif index == 5:
+        disease_predict = "Pigmented benign keratosis"
+    elif index == 6:
+        disease_predict = "Seborrheic keratosis"
+    elif index == 7:
+        disease_predict = "Squamous cell carcinoma"
+    elif index == 8:
+        disease_predict = "Vascular lesion"
+    elif index == 8:
+        disease_predict = "Normal skin"
+
+    try:
+        if disease_predict != "Normal skin":
+            response = openai.Completion.create(model="text-davinci-003", prompt=disease_predict, temperature=0.2,
+                                                max_tokens=100, top_p=0.5, frequency_penalty=1.65,
+                                                presence_penalty=0.6,
+                                                stop=["User: "])
+            completion = disease_predict + " "
+            completion += response["choices"][0]["text"].replace("\n\n", "")
+            completion = re.sub(r"Bot: ", "", completion)
+        else:
+            completion = "No cancer "
+            completion = re.sub(r"Bot: ", "", completion)
+        botResponse = {
+            "message": completion,
+            "type": "bot",
+            "timestamp": datetime.datetime.now()
+        }
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail="OpenAI API error")
+    return botResponse
